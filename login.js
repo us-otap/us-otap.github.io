24af5f36506e0022546d2bfd832dd20c26cdb6f3
Jturkey5 <127263710+Jturkey5@users.noreply.github.com>
2023-09-02 23:43:21 -0800

    fix2 i hope this will work

--- login.js
+++ login.js
@@ -22,30 +22,37 @@ console.log(app);
 const db = getDatabase(app);
 
 document.getElementById("register").addEventListener("click", function() {
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const email =  document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    createUserWithEmailAndPassword(auth, email, password)
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const email =  document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
 
-    .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        writeUserData(db, user.uid, firstName, lastName, email);
+      const volunteerHoursElement = document.createElement('p');
+      volunteerHoursElement.id = 'volunteerHours';
+      volunteerHoursElement.innerHTML = 'Volunteer Hours: 0 hours';
+      document.getElementById('user-info').appendChild(volunteerHoursElement);
 
-        document.getElementById("first-name").value = '';
-        document.getElementById("last-name").value = '';
-        document.getElementById("email").value = '';
-        document.getElementById("password").value = '';
-        alert(user.email + " registered successfully");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      alert(error);
-    });		  		  
+      writeUserData(db, user.uid, firstName, lastName, email);
+
+      document.getElementById("first-name").value = '';
+      document.getElementById("last-name").value = '';
+      document.getElementById("email").value = '';
+      document.getElementById("password").value = '';
+      alert(user.email + " registered successfully");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    alert(error);
+  });		  		  
 });
+
 
 document.getElementById("login").addEventListener("click", function() {
     const email =  document.getElementById("login_email").value;
@@ -63,23 +70,6 @@ z    })
       alert(errorMessage);
     });		  		  
 }); 
-
-export function updateDashboard(db, userID) {
-  const data = ref(db, 'users/' + userID);
-  onValue(data, (snapshot) => {
-      const userData = snapshot.val();
-      console.log(userData);
-      const volunteerHours = userData["volunteerHours"] || 0;
-      
-      // Update user info
-      document.getElementById('user-email').textContent = userData["email"];
-      document.getElementById('user-name').textContent = `${userData["firstName"]} ${userData["lastName"]}`;
-
-      // Update volunteer hours
-      document.getElementById('volunteer-hours').textContent = `Volunteer Hours: ${volunteerHours} hours`;
-  });
-}
-
 
 
 
