502f8d37e6ee553f9903a382358cb598c575c1d6
Jturkey5 <127263710+Jturkey5@users.noreply.github.com>
2023-09-03 16:33:18 -0800

    ewwefds
    d

--- login.js
+++ login.js
@@ -20,68 +20,69 @@ const auth = getAuth();
 const db = getDatabase(app);
 
 document.getElementById("register").addEventListener("click", function() {
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const email =  document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const email =  document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    const volunteerHours = 0;
-    const volunteerHoursId = "volunteerHours";
+  const volunteerHours = 0;
+  const volunteerHoursId = "volunteerHours";
 
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-
-        const userData = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            volunteerHours: volunteerHours,
-            password: password,
-        };
-
-        set(ref(db, 'users/' + user.uid), userData); 
-        const volunteerHoursElement = document.getElementById('volunteerHours');
-    if (volunteerHoursElement) {
-        volunteerHoursElement.innerText = `${volunteerHoursId.charAt(0).toUpperCase() + volunteerHoursId.slice(1)}: ${volunteerHours} hours`;
-    } else {
-        console.error("Element with id 'volunteerHours' not found in the DOM.");
-    }
-        document.getElementById("first-name").value = '';
-        document.getElementById("last-name").value = '';
-        document.getElementById("email").value = '';
-        document.getElementById("password").value = '';
-        alert(user.email + " registered successfully");
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        alert(error);
-    });
-});
-
-
-
-document.getElementById("login").addEventListener("click", function() {
-    const email =  document.getElementById("login_email").value;
-    const password = document.getElementById("login_password").value;
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
       const user = userCredential.user;
       console.log(user);
-      window.location.href = 'user_dashboard.html';
-z    })
-    .catch((error) => {
+
+      const userData = {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          [volunteerHoursId]: volunteerHours,
+          password: password,
+      };
+
+      set(ref(db, 'users/' + user.uid), userData);
+
+      // Ensure the DOM is ready before manipulating the "volunteerHours" element
+      document.addEventListener("DOMContentLoaded", function() {
+          const volunteerHoursElement = document.getElementById(volunteerHoursId);
+          if (volunteerHoursElement) {
+              volunteerHoursElement.innerText = `${volunteerHoursId.charAt(0).toUpperCase() + volunteerHoursId.slice(1)}: ${volunteerHours} hours`;
+          } else {
+              console.error(`Element with id '${volunteerHoursId}' not found in the DOM.`);
+          }
+      });
+
+      document.getElementById("first-name").value = '';
+      document.getElementById("last-name").value = '';
+      document.getElementById("email").value = '';
+      document.getElementById("password").value = '';
+      alert(user.email + " registered successfully");
+  })
+  .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorMessage);
-      alert(errorMessage);
-    });		  		  
-}); 
+      alert(error);
+  });
+});
 
-
+document.getElementById("login").addEventListener("click", function() {
+  const email =  document.getElementById("login_email").value;
+  const password = document.getElementById("login_password").value;
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    window.location.href = 'user_dashboard.html';
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    alert(errorMessage);
+  });
+});
 
 
 
